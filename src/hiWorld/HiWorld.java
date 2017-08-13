980359860e1a4ae301554088430b1aec9957c177
zyr <1041953637@qq.com>
2017-08-13 13:43:20 +0800

    test

--- src/hiWorld/HiWorld.java
+++ src/hiWorld/HiWorld.java
@@ -2,6 +2,6 @@ package hiWorld;
 
 public class HiWorld {
     public static void main(String[] args) {
-        System.out.println("HiWorld!");
+        System.out.println("I like loli! ");
     }
 }
